84fd5d25252cd06cada55e0509ebcccb7a33824d
son-hunseo <thsgnstj0512@gmail.com>
2025-12-29 22:38:52 +0900

    [feat] array

---
+++ 03-datastructure-memory/01-array.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers [2]int // 배열의 크기가 정해지면 크기를 바꿀 수 없다.
+	fmt.Printf("%+v\n", numbers)
+
+	numbers[0] = 1
+	numbers[1] = 2
+	fmt.Printf("%+v\n", numbers)
+
+	primes := [4]int{2, 3, 5, 7}
+	fmt.Printf("%+v\n", primes)
+	primes[3] = 11
+	fmt.Printf("%+v\n", primes)
+
+	for i := 0; i < len(primes); i++ {
+		fmt.Printf("%d \n", primes[i])
+	}
+
+	var matrix [2][3]int
+	matrix[0][0] = 1
+	matrix[0][1] = 2
+	matrix[1][2] = 3
+
+	fmt.Printf("%+v\n", matrix)
+}
